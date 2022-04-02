98292d6de01c6a0d892f2a57899eae5cc94cef63
keshavj2302 <90474590+keshavj2302@users.noreply.github.com>
2022-04-02 13:49:09 +0530

    connecting with mongodb Atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/befriends_Development');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/befriends_Development');
 const db = mongoose.connection;
 
 db.on('error', function(err){
